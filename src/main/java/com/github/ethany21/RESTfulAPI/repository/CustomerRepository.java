22f6255d7be49e5a6ea30ae9313d33fbcf9f9587
ethany21 <imwoodam@hanmail.net>
2021-03-27 15:45:26 +0900

    Added Repository annotation

--- src/main/java/com/github/ethany21/RESTfulAPI/repository/CustomerRepository.java
+++ src/main/java/com/github/ethany21/RESTfulAPI/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.github.ethany21.RESTfulAPI.repository;
 
 import com.github.ethany21.RESTfulAPI.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
